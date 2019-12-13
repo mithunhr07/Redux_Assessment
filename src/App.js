360296e4884b8d3dc9388178e130cee58aee335b
mithun <mithunhrm07@gamil.com>
2019-12-13 17:42:27 +0530

    remaining

--- src/App.js
+++ src/App.js
@@ -3,20 +3,47 @@ import './App.css';
 import Login from './Components/Login';
 // import submit from './Submit/submit';
 // import submitvalidationform from './Submit/submitvalidationform';
-import Register from './Components/Register';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import PostForm from './Components/PostForm';
+// import AllPost from './Components/AllPost';
+import PostForm from './Components/PostForm';
+import AllPost from './Components/AllPost';
+// import Register from './Components/Register';
+import { BrowserRouter as Router, Route , Switch} from 'react-router-dom';
                      
 function App() {
   return (
     <div className="App">
    <Router> 
-   <switch>
-   <Route exact path='/' component={Login}></Route>
-   <Route exact path='/Register' component={Register}></Route>
-   </switch>
+   <Switch>
+   {/* <Route exact path='/' component={Login}></Route> */}
+   {/* <Route exact path='/Register' component={Register}></Route> */}
+   {/* <Route exact path='/Users' component={Users}></Route> */}
+   {/* <Route exact path='/PostForm' component={PostForm}></Route> */}
+   {/* <Route exact path='/AllPost' component={AllPost}></Route> */}
+   
+   <Route exact path='/' component={PostForm}></Route>
+   <Route exact path='/AllPost' component={AllPost}></Route>
+   </Switch>
  </Router>
     </div>
   );
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+// import PostForm from './Components/PostForm';
+// import AllPost from './Components/AllPost';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <PostForm/>
+//         {/* <AllPost/> */}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
