4964ebcdc55cdaefc0a11de05a19470fe0068574
mithun <mithunhrm07@gamil.com>
2019-12-16 11:23:05 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import './App.css';
 import Login from './Components/Login';
-// import submit from './Submit/submit';
-// import submitvalidationform from './Submit/submitvalidationform';
-// import PostForm from './Components/PostForm';
-// import AllPost from './Components/AllPost';
 import PostForm from './Components/PostForm';
 import AllPost from './Components/AllPost';
-// import Register from './Components/Register';
+import Register from './Components/Register';
 import { BrowserRouter as Router, Route , Switch} from 'react-router-dom';
                      
 function App() {
@@ -15,14 +11,11 @@ function App() {
     <div className="App">
    <Router> 
    <Switch>
-   {/* <Route exact path='/' component={Login}></Route> */}
-   {/* <Route exact path='/Register' component={Register}></Route> */}
+   <Route exact path='/' component={Login}></Route>
+   <Route exact path='/Register' component={Register}></Route>
    {/* <Route exact path='/Users' component={Users}></Route> */}
    {/* <Route exact path='/PostForm' component={PostForm}></Route> */}
    {/* <Route exact path='/AllPost' component={AllPost}></Route> */}
-   
-   <Route exact path='/' component={PostForm}></Route>
-   <Route exact path='/AllPost' component={AllPost}></Route>
    </Switch>
  </Router>
     </div>
@@ -31,19 +24,3 @@ function App() {
 
 export default App;
 
-// import React, { Component } from 'react';
-// import PostForm from './Components/PostForm';
-// import AllPost from './Components/AllPost';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <PostForm/>
-//         {/* <AllPost/> */}
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
